119ac339523fcae2380d105d36a3add8f7c9700a
Carmelia Shito <carmshito@hotmail.com>
2023-04-07 16:41:02 -0400

    fix console.log to include array1 and array2

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -12,8 +12,8 @@ const eqArrays = function(array1, array2) {
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2) === true) {
-    console.log(`✅✅✅ Assertion Passed`);
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
   } else {
-    console.log(`❌❌❌ Assertion Failed`);
+    console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
   }
 };
