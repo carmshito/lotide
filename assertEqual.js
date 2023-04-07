87e02e1aa8ab0b75dee134176f3d4750c6f527d9
Carmelia Shito <carmshito@hotmail.com>
2023-04-07 17:28:50 -0400

    removed else

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
+  if (actual !== expected) {
     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+    return;
   }
+  console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
 };
 
 assertEqual("Lighthouse Labs", "Bootcamp");
