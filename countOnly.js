c0fa48fc6e5d04a1475c60177ec179dd8c795aac
Carmelia Shito <carmshito@hotmail.com>
2023-04-07 18:01:38 -0400

    fix to remove else

--- countOnly.js
+++ countOnly.js
@@ -10,13 +10,12 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {
     // console.log(item);
-    if (itemsToCount[item]) {
-      if (results[item]) {
-        results[item] += 1;
-      } else {
-        results[item] = 1;
-      }
+    if (!itemsToCount[item]) {
+      continue;
     }
+    if (!results[item]) {
+      results[item] = 0;
+    } results[item]++;
   }
   return results;
 };
