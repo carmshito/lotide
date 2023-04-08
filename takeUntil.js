dbc367c2401316f2db54e127c456c8f942382b61
Carmelia Shito <carmshito@hotmail.com>
2023-04-07 23:55:06 -0400

    removed spaces

--- takeUntil.js
+++ takeUntil.js
@@ -33,8 +33,6 @@ const takeUntil = function(array, callback) {
   return result;
 };
 
-
-
 // TEST CODES
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
@@ -45,4 +43,5 @@ console.log('---');
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+
 assertArraysEqual(results2, ['I\'ve', 'been', 'to', 'Hollywood']);
