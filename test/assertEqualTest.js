725228e71d60d3c6a22e67563f2021f806eb1a54
Carmelia Shito <carmshito@hotmail.com>
2023-04-10 20:19:05 -0400

    add test directory and files

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(50, 1);
+assertEqual(1, "Hello");
+assertEqual("Hello", "Hello");
+assertEqual("Hello", "hello");
