7c7fb116070a59b05f31cb905873ea78a16eb5d6
Carmelia Shito <carmshito@hotmail.com>
2023-04-10 20:18:44 -0400

    refactor code

--- assertEqual.js
+++ assertEqual.js
@@ -6,9 +6,4 @@ const assertEqual = function(actual, expected) {
   console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(50, 1);
-assertEqual(1, "Hello");
-assertEqual("Hello", "Hello");
-assertEqual("Hello", "hello");
+module.exports = assertEqual;
