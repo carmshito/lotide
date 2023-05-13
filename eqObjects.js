fc1481d7d9e6c97206ecc97a59cc5d03c96ca90d
Carmelia Shito <carmshito@hotmail.com>
2023-05-13 18:21:10 -0400

    add space after equal sign

--- eqObjects.js
+++ eqObjects.js
@@ -1,8 +1,8 @@
 const eqArrays = require("./eqArrays");
 
 const eqObjects = function(object1, object2) {
-  let array1 = Object.keys(object1); //  array of keys
-  let array2 = Object.keys(object2); //  array of keys
+  let array1 = Object.keys(object1);
+  let array2 = Object.keys(object2);
   if (array1.length !== array2.length) {
     return false;
   }
